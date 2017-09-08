1eb9a0d7bb9d04939688f8e8877fd817b5ae3391
gowsi98 <31766297+gowsi98@users.noreply.github.com>
2017-09-08 16:49:15 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>home page</title>
+</head>
+<body>
+<nav>
+<a href="home.html">home</a>
+<a href="aboutas.html">aboutas</a>
+<a href="context.html">context</a>
+<a href="feedback.html">feedback</a>
+</nav>
+<style>
+nav{
+padding:10px;
+}
+a{border:1pxsolid red;padding=10px;
+}
+</style>
+</body>
+</html>
